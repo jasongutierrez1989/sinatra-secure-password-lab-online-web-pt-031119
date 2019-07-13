4cfdd9baaea25ba7c25c6cc28a901f73643fac3e
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-07-13 22:41:45 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < Sinatra::Base
     if params[:username]=="" || params[:password]==""
       redirect to '/failure'
     end
-    
+
     user = User.new(:username => params[:username], :password => params[:password])
 		if user.save
 	    redirect "/login"
