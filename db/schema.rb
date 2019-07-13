e34ef1204e687b54b8794a9510e662b81483962a
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-07-13 22:39:51 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190713223542) do
+ActiveRecord::Schema.define(version: 20190713223917) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
+    t.string "password_digest"
   end
 
 end
